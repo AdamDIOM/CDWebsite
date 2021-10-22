0ebd81e6caa2621b4231de672e046685b8eb3be1
AdamDIOM <adam.drummond9@gmail.com>
2021-10-22 13:25:14 +0100

    created site, added cover stock image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { Container, Navbar } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar>
+        <Container>
+          <Navbar.Brand href="/">Home</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <div className="BackImage">
+        <div className="ImageCover">
+          <div className="Hello">
+            Hello <br/>
+            Scroll down to read more
+          </div>
+        </div>
+      </div>
+      hi
     </div>
   );
 }
