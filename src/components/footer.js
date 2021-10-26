862c2851b6ff11548a5aff217ac11b4445f7688f
AdamDIOM <adam.drummond9@gmail.com>
2021-10-26 20:52:04 +0100

    implemented footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,4 @@
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import './footer.css';
 
 
@@ -6,7 +6,89 @@ function Footer() {
   return (
     <div className="Footer">
       <Container>
-        Footer
+        <Row>
+          <Col md={4} sm={12}>
+            <h4>Contact Us</h4>
+            <Row>
+              <Col sm={2}>
+                p
+              </Col>
+              <Col sm={10} className="footerBullet">
+              +447624 341757
+              </Col>
+            </Row>
+            <Row>
+              <Col sm={2}>
+                e
+              </Col>
+              <Col sm={10} className="footerBullet">
+              <a href="mailto:craig@cdc.org" className="email">craig@cdc.org</a>
+              </Col>
+            </Row>
+            <Row>
+              <Col sm={2}>
+                a
+              </Col>
+              <Col sm={10} className="footerBullet">
+              address
+              </Col>
+            </Row>
+          </Col>
+          <Col md={4} sm={12}>
+            <h4>Debt Management</h4>
+            <Row>
+              <Col sm={2}>
+                a
+              </Col>
+              <Col sm={10} className="footerBullet">
+                text
+              </Col>
+            </Row>
+            <Row>
+              <Col sm={2}>
+                b
+              </Col>
+              <Col sm={10} className="footerBullet">
+                text
+              </Col>
+            </Row>
+            <Row>
+              <Col sm={2}>
+                c
+              </Col>
+              <Col sm={10} className="footerBullet">
+                text
+              </Col>
+            </Row>
+          </Col>
+          <Col md={4} sm={12}>
+            <h4>Company Doctor</h4>
+            <Row>
+              <Col sm={2}>
+                d
+              </Col>
+              <Col sm={10} className="footerBullet">
+                text
+              </Col>
+            </Row>
+            <Row>
+              <Col sm={2}>
+                e
+              </Col>
+              <Col sm={10} className="footerBullet">
+                text
+              </Col>
+            </Row>
+            <Row>
+              <Col sm={2}>
+                f
+              </Col>
+              <Col sm={10} className="footerBullet">
+                text
+              </Col>
+            </Row>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
